ef341e8e949745b6c2d8f1055090779c5bf6eaa1
Htocki <pasztownia@gmail.com>
2020-04-19 11:41:23 +0300

    Реализована часть программ 1-й части 2-й лабораторной.

---
+++ L2/Part1/Whiteline/src/Main.cpp
@@ -0,0 +1,24 @@
+#include <GL/glut.h>
+
+void Draw() {
+  glClear(GL_COLOR_BUFFER_BIT);
+  glColor3d(1.0, 1.0, 1.0);
+  glLineWidth(3);
+  glBegin(GL_LINES);
+    glVertex2d(-0.5, 0.8);
+    glVertex2d(0.5, -0.8);
+  glEnd();
+  glFinish();
+}
+
+int main(int argc, char** argv) {
+  glutInit(&argc, argv);
+  glutInitDisplayMode(GLUT_RGB);
+  glutInitWindowPosition(200, 200);
+  glutInitWindowSize(240, 300);
+  glutCreateWindow("Whiteline");
+  glClearColor(0.0, 0.0, 0.5, 1.0);
+  glutDisplayFunc(Draw);
+  glutMainLoop();
+  return 0;
+}
