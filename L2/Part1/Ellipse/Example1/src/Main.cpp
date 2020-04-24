c917e9408c845cf94ff206bfa1efb1b63ba93d95
Htocki <pasztownia@gmail.com>
2020-04-24 14:50:21 +0300

    Добавлены примеры с рисованием элипса.

---
+++ L2/Part1/Ellipse/Example1/src/Main.cpp
@@ -0,0 +1,48 @@
+#include <GL/glut.h>
+
+#include <cmath>
+
+constexpr double a { 0.7 }, b { 0.4 };
+constexpr int n { 100 };
+
+void Axis() {
+  glColor3d(1.0, 0.0, 0.0);
+  glLineWidth(1);
+  glBegin(GL_LINES);
+    glVertex2d(0.0, 1.0);
+    glVertex2d(0.0, -1.0);
+    glVertex2d(1.0, 0.0);
+    glVertex2d(-1.0, 0.0);
+  glEnd();
+}
+
+void Ellipse() {
+  glColor3d(1.0, 1.0, 1.0);
+  glLineWidth(3);
+  glBegin(GL_LINE_LOOP);
+  for (auto i { 0 }; i < n; ++i) {
+    glVertex2d(
+      a * std::sin(2 * 3.1416 * i / n),
+      b * std::cos(2 * 3.1416 * i / n));
+  }
+  glEnd();
+}
+
+void Draw() {
+  glClear(GL_COLOR_BUFFER_BIT);
+  Axis();
+  Ellipse();
+  glFinish();
+}
+
+int main(int argc, char** argv) {
+  glutInit(&argc, argv);
+  glutInitDisplayMode(GLUT_RGB);
+  glutInitWindowPosition(200, 200);
+  glutInitWindowSize(400, 400);
+  glutCreateWindow("Ellipse");
+  glClearColor(0.0, 0.0, 0.5, 1.0);
+  glutDisplayFunc(Draw);
+  glutMainLoop();
+  return 0;
+}
