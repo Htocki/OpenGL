0c484b99652841dc88bf616372d95314b5a7847e
Htocki <pasztownia@gmail.com>
2020-05-12 13:58:23 +0300

    Исправлена ошибка, из-за которой на графике не отображалась ось Y. Все затронутые изненениями скришоты - заменены.

--- L3/Part1/src/Main.cpp
+++ L3/Part1/src/Main.cpp
@@ -11,13 +11,13 @@ void RenderScene() {
   glClear(GL_COLOR_BUFFER_BIT);
   glColor3f(1.0, 0.0, 0.0);
   glBegin(GL_LINES);
-    glVertex2f(0.0, 0.0);
-    glVertex2f(cx,  0.0);
+    glVertex2f(0.001, 0.001);
+    glVertex2f(cx, 0.001);
   glEnd();
 
   glBegin(GL_LINES);
-    glVertex2f(0.0, 0.0);
-    glVertex2f(0.0,  cy);
+    glVertex2f(0.001, 0.001);
+    glVertex2f(0.001, cy);
   glEnd();
 
   glColor3f(0.5, 0.8, 0.4);
